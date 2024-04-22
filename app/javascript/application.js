1607d308e578d0a5c8901c9e5de42beac4bbafd6
野口 瑠星 <noguchi.ryusei@k-idea.local>
2024-04-22 13:07:29 +0900

    vue導入

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,46 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import * as Vue from "vue";
+
+const App = Vue.createApp({
+    data(){
+        return {
+            text: 'OFF',
+            isShow: false
+        }
+    },
+    methods:{
+        viewSearchChange() {
+            this.isShow = !this.isShow;
+            this.text = this.isShow ? "ON" : "OFF";
+            },
+          },
+    
+})
+App.mount("#app");
+
+const Example = Vue.createApp({
+    el: '#vue_example',
+  
+    template: `<div>
+      <p>{{ message }}</p>
+      <input v-model="message" />
+      <button v-on:click="reverse()">Reverse</button>
+      </div>`,
+  
+    data() {
+        return{
+      message: 'Hello Vue.js!',
+    }
+    },
+  
+    methods: {
+      reverse: function () {
+        this.message = this.message.split('').reverse().join('')
+      },
+    },
+  });
+
+  Example.mount("#vue_example");
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "vue@3", to: "https://cdn.skypack.dev/vue@3", preload: true
+pin "application", preload: true
+# pin "vue@3", to: "https://cdn.skypack.dev/vue@3", preload: true
+pin "vue", to: "vue--dist--vue.esm-browser.js.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "vue/dist/vue.esm-browser.js", to: "vue--dist--vue.esm-browser.js.js" # @3.4.23
