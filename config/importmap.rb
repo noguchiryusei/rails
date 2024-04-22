2e272ff312a237de0ec206ad3e48022359084bca
野口 瑠星 <noguchi.ryusei@k-idea.local>
2024-04-22 13:06:39 +0900

    アプリ作成

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+pin "vue@3", to: "https://cdn.skypack.dev/vue@3", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :tweets do
+    root to: "tweets#index"
+    get "new" => "tweets#new", as: :new_tweet_path
+    get "test" => "tweets#test"
+    # get "/index" => "tweets#index"
 
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
